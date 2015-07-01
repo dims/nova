b9c11e429d08a07f85763380ecdeb2664190252b
Davanum Srinivas <davanum@gmail.com>
2015-07-01 13:44:32 -0400

    add --no-summary to subunit-trace

--- tools/pretty_tox.sh
+++ tools/pretty_tox.sh
@@ -12,5 +12,5 @@ TESTRARGS=$1
 if [[ "$TESTARGS" =~ "until-failure" ]]; then
     python setup.py testr --slowest --testr-args="$TESTRARGS"
 else
-    python setup.py testr --slowest --testr-args="--subunit $TESTRARGS" | subunit-trace -f
+    python setup.py testr --slowest --testr-args="--subunit $TESTRARGS" | subunit-trace -f --no-summary
 fi
